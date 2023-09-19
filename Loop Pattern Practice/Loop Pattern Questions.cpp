5511bc5c05dc1e7eaf880cff79ad6e37f0a8f7e7
HP <HP@DESKTOP-4IO1188>
2023-09-19 06:59:44 -0700

    no

--- Loop Pattern Practice/Loop Pattern Questions.cpp
+++ Loop Pattern Practice/Loop Pattern Questions.cpp
@@ -18,5 +18,14 @@ int main() {
 		}
 		cout << endl;
 	}*/
+	// 3rd Pattern 
+	int q = 0;
+	for (int i = 0; i <= rows; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << q;
+			q++;
+		}
+		cout << endl;
+	}
 	return 0;
 }
