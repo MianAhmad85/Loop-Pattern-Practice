5fb3c0414f01ddb04fa3eddc8f2c2e8573a33bab
HP <HP@DESKTOP-4IO1188>
2023-09-19 06:55:08 -0700

    2nd pattern

--- Loop Pattern Practice/Loop Pattern Questions.cpp
+++ Loop Pattern Practice/Loop Pattern Questions.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
 using namespace std;
 int main() {
-	// 1st Pattern
 	int rows = 0;
 	cout << "Enter no of rows"<<endl;
-	cin >> rows;
-	for (int i = 0; i <= rows; i++) {
+    cin >> rows;
+	// 1st Pattern
+	/*for (int i = 0; i <= rows; i++) {
 		for (int j = 0; j <= i; j++) {
 			cout << "*";
+		}
+		cout << endl;
+	}*/
+	//2nd Pattern
+	for (int i = 0; i <= rows; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << i;
 		}
 		cout << endl;
 	}
