0491a74eed47e1367b09eb7c7ec636cf74cfad53
HP <HP@DESKTOP-4IO1188>
2023-09-19 06:52:42 -0700

    1st pattern

--- Loop Pattern Practice/Loop Pattern Questions.cpp
+++ Loop Pattern Practice/Loop Pattern Questions.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main() {
-
+	// 1st Pattern
+	int rows = 0;
+	cout << "Enter no of rows"<<endl;
+	cin >> rows;
+	for (int i = 0; i <= rows; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
 }
